8abe76ca7a9face911214441bc636b3caa74416c
Nikola Stamatovic <stamatmail@gmail.com>
2026-08-06 20:19:55 +0200

    Build version 1.1.1

--- dist/hydrargyri.mjs
+++ dist/hydrargyri.mjs
@@ -1,4 +1,4 @@
-/* hydrargyri v1.1.0 | https://stamat.github.io/hydrargyri/ | MIT License */
+/* hydrargyri v1.1.1 | https://stamat.github.io/hydrargyri/ | MIT License */
 var __defProp = Object.defineProperty;
 var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
 var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
